11741c93ef0efb9a1288405b0426a759b1fb571b
Parneet kaur Jattana <Parneet kaur Jattana@192.168.0.24>
2019-05-28 00:44:17 -0400

    practice

---
+++ javaBasic/Tim1.java
@@ -0,0 +1,33 @@
+
+
+public class Tim1 
+{
+
+public static void main(String[] args)
+{
+//String x;
+//parneet();
+//manpreet("parneet");
+//x = parneet1();
+//System.out.println("how are you, "+x);
+int y ;
+y = manpreet1(12);
+System.out.println("work " +y);
+}
+public static void parneet()
+{
+System.out.println("How are you ?");
+}
+public static void manpreet(String args)
+{
+System.out.println("how are you, " +args);
+}
+public static String parneet1()
+{
+return "parneet";
+}
+public static int manpreet1(int arg1)
+{
+return arg1;
+}
+}
